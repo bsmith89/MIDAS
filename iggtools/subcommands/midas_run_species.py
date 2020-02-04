381d2cda7efdb761ea7a2717a65de82e7eef8fe8
Byron Smith <me@byronjsmith.com>
2020-02-04 10:23:13 -0800

    Download reference data to tempdir not ./

--- iggtools/subcommands/midas_run_species.py
+++ iggtools/subcommands/midas_run_species.py
@@ -267,7 +267,12 @@ def midas_run_species(args):
     command(f"rm -rf {tempdir}")
     command(f"mkdir -p {tempdir}")
 
-    markers_db_files = multithreading_map(download_reference, [f"s3://microbiome-igg/2.0/marker_genes/phyeco/phyeco.fa{ext}.lz4" for ext in ["", ".bwt", ".header", ".sa", ".sequence"]] + ["s3://microbiome-igg/2.0/marker_genes/phyeco/phyeco.map.lz4"])
+    markers_db_files = multithreading_map(
+        lambda s: download_reference(s, local_dir=tempdir),
+        [f"s3://microbiome-igg/2.0/marker_genes/phyeco/phyeco.fa{ext}.lz4"
+         for ext in ["", ".bwt", ".header", ".sa", ".sequence"]] +
+        ["s3://microbiome-igg/2.0/marker_genes/phyeco/phyeco.map.lz4"]
+                                          )
 
     db = UHGG()
     species_info = db.species
